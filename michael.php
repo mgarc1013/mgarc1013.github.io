7f668a39787565315cb430ddeb0fd981141f3183
mgarc1013 <35808821+mgarc1013@users.noreply.github.com>
2019-04-30 20:16:09 -0400

    init commit

---
+++ michael.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Michael Garcia</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="M.png" />
+</head>
+
+<body>
+    <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#projects">Portfolio</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+
+    <div id="lead">
+        <div id="lead-content">
+            <h1>Michael Garcia</h1>
+            <h2>Web Developer and Designer</h2>
+            <a href="Garcia.docx" download class="btn-rounded-white">Download Resume</a>
+            
+        </div>
+        <!-- End #lead-content -->
+
+        <div id="lead-overlay"></div>
+
+       
+        <!-- End #lead-down -->
+    </div>
+    <!-- End #lead -->
+
+    <div id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2 class="heading">About Me</h2>
+                    </div>
+                    <div class="col-md-8">
+                        <p>
+                            I'm Michael, a recent graduate from the University of Central Florida and I make cool stuff on the web. <br>Below, you'll find some of my featured work. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <section class="wrapper" id="projects">
+        <div class="container-fostrap">
+            <div>
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="https://github.com/mgarc1013/HH">
+                                <img src="pita.jpg" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="https://github.com/mgarc1013/HH"> Rebranded Hummus House
+                                      </a>
+                                    </h4>
+                                    <p class="">
+                                        Complete rebrand of the restaurant that I manage. Original link: <a href="https://hummushouseucf.com/?gclid=CjwKCAjwtYXmBRAOEiwAYsyl3BN3TrBkbQBsRKcWStMU0dWkEzMVJG-znvgvDP3x-9sWX6l3aRdpNhoC8MAQAvD_BwE">hummushouseucf.com</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="https://github.com/mgarc1013/Xeno">
+                                <img src="xeno.jpg" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="https://github.com/mgarc1013/Xeno"> XENO API 
+                                      </a>
+                                    </h4>
+                                    <p class="">
+                                        Simple application utilizing Google Maps API that updates UFO sightings around Central Florida.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="https://github.com/mgarc1013/Fit">
+                                <img src="fit.jpg" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="https://github.com/mgarc1013/Fit">FitLife 
+                                      </a>
+                                    </h4>
+                                    <p class="">
+                                        Fitness tutorials website developed using PHP and SQL and includes a working database.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="wrapper">
+        <div class="container-fostrap2">
+            <div>
+            </div>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div style="visibility: hidden" class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html">
+                                <img src="laika.png" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="http://www.fostrap.com/2016/03/bootstrap-3-carousel-fade-effect.html"> Laika
+                                      </a>
+                                    </h4>
+                                    <p class="">
+                                        A children's education website that was developed with a small team of designers.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="https://github.com/mgarc1013/laika">
+                                <img src="laika.png" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="https://github.com/mgarc1013/laika"> Laika
+                                        </a>
+                                    </h4>
+                                    <p class="">
+                                        A children's education website that was developed with a small team of designers.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="visibility: hidden" class="col-xs-12 col-sm-4">
+                            <div class="card">
+                                <a class="img-card" href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html">
+                                <img src="flair.jpg" />
+                              </a>
+                                <div class="card-content">
+                                    <h4 class="card-title">
+                                        <a href="http://www.fostrap.com/2016/03/5-button-hover-animation-effects-css3.html"> Placeholder 
+                                      </a>
+                                    </h4>
+                                    <p class="">
+                                        tutorials button hover animation, although very much a hover button is very beauti...
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+    <div id="contact">
+        <h2>Looking to start a project? Let's talk.</h2>
+        <div id="contact-form">
+            <form method="POST" action="https://formspree.io/mgarc1013@gmail.com">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_replyto" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
+
+
+    <footer class="section">
+            <div class="container">
+              <ul>
+                <li><a target="_blank" class="facebook" href="https://www.facebook.com/profile.php?id=1711672230"><i class="fa fa-fw fa-2x fa-inverse fa-facebook"></i></a></li>
+                <li><a target="_blank" class="linkedin" href="https://www.linkedin.com/in/michael-garcia-739524177/"><i class="fa fa-fw fa-2x fa-inverse fa-linkedin"></i></a></li>
+                <li><a target="_blank" class="github" href="https://github.com/mgarc1013"><i class="fa fa-fw fa-2x fa-inverse fa-github"></i></a></li>
+              </ul>
+            </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="scripts.min.js"></script>
+</body>
+
+</html>
